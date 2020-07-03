b0eb90db52a76df4fdc66a826d3804a93cec4a71
CodenificenT <xristianty@gmail.com>
2020-07-03 00:45:56 -0400

    new concept

---
+++ chrishawkes/arrays.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myArray [5]int
+
+	myArray[0] = 1
+	myArray[1] = 2
+	myArray[2] = 3
+	myArray[3] = 4
+	myArray[4] = 5
+
+	fmt.Println(myArray)
+
+	myArray1 := [2]string{"Two", "Five"}
+
+}
